e172259230a29a875b1a4ec1316adac665e178d9
irizzcodes <gennreypiao@yahoo.com>
2025-03-11 09:39:32 +0800

    add mainImage json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import CardLayout from "../components/CardLayout";
 import Footer from "../components/Footer";
+import MainImage from "../components/MainImage";
+import { useState } from "react";
 import "../css/styles.css";
 function Home() {
+    const [mainImages, setMainImages] = useState([
+        {
+        image: "/assets/cecilia.png",
+        },
+      ]);
     
 
     return <div className="home">
+        <MainImage image={mainImages[0].image} />
         <CardLayout />
         <Footer />
     </div>
